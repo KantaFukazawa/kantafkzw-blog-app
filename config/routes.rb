116d6c0756de801529179c0f1be0a9c555b27581
深澤　寛太 <kantafkzw@gmail.com>
2020-09-12 16:18:26 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   root to: 'home#index'
   get '/about' => 'home#about'
+=======
+  root to: 'articles#index'
+
+  resources :articles, only: [:show]
+>>>>>>> parent of fe0af3c... no message
 end
