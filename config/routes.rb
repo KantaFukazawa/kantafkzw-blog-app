71d0f7658dc555ee4a2ddfc8d5dd6c64a37113b7
深澤　寛太 <kantafkzw@gmail.com>
2020-12-23 11:26:59 +0900

    namespaceとscope

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,8 @@ Rails.application.routes.draw do
 
   resource :timeline, only: [:show]
 
-  resources :articles do
-    resources :comments, only: [:index, :new, :create]
-    resource :like, only: [:show, :create, :destroy]
-  end
+  resources :articles 
+
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
@@ -23,4 +21,11 @@ Rails.application.routes.draw do
 
   resource :profile, only: [:show, :edit, :update]
   resources :favorites, only: [:index]
+
+  namespace :api, defaults: {format: :json} do
+    scope '/articles/:article_id' do
+      resources :comments, only: [:index, :create]
+      resource :like, only: [:index, :create, :destroy]
+    end
+  end
 end
